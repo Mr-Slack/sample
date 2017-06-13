fd79e8cf8f61d330a8b832489aa9b4cf6b17a090
松下裕紀 <j.hiroki.matsushita@gmail.com>
2017-06-14 08:40:20 +0900

    VideoJS動作確認サンプル追加

---
+++ app/controllers/videojs_controller.rb
@@ -0,0 +1,4 @@
+class VideojsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'home', to: 'form#index'
   post 'form/search', to: 'form#search'
   resources :books
+  get 'videoJS', to: 'videojs#index'
 end
